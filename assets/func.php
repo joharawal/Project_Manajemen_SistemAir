eabeed0319b1bef6998d496a55f630be8f5a18c3
joharawal <awaljohar0@gmail.com>
2026-04-13 14:49:06 +0700

    update func.php

--- assets/func.php
+++ assets/func.php
@@ -1,14 +1,56 @@
+<!-- //<?php
+//class klas_air {
+ //   private $host = "localhost";
+ //   private $user = "teemyid_sembarangsek"; 
+ //   private $pass = "bebasaja123";
+ //   private $db   = "teemyid_kelompok8";
+
+ //   private $host = "localhost";
+ //   private $user = "root"; 
+ //   private $pass = "";
+ //   private $db   = "kelompok8.te24e.my.id";
+
+    //public function koneksi() {
+    //    $koneksi = mysqli_connect($this->host, $this->user, $this->pass, $this->db);
+    //    if (!$koneksi) {
+     //       die("Koneksi database gagal: " . mysqli_connect_error());
+    //    }
+    //    return $koneksi;
+    //}
+
+    //public function dt_user($username) {
+    //    $koneksi = $this->koneksi();
+    //    $username = mysqli_real_escape_string($koneksi, $username);
+   //     $query = mysqli_query($koneksi, "SELECT nama, username, level FROM user WHERE username = '$username'");
+   //     return mysqli_fetch_row($query); 
+   // }
+//}
+//?> -->
+
 <?php
 class klas_air {
-    // private $host = "localhost";
-    // private $user = "teemyid_sembarangsek"; 
-    // private $pass = "bebasaja123";
-    // private $db   = "teemyid_kelompok8";
 
-    private $host = "localhost";
-    private $user = "root"; 
-    private $pass = "";
-    private $db   = "kelompok8.te24e.my.id";
+    private $host;
+    private $user;
+    private $pass;
+    private $db;
+
+    public function __construct() {
+        // cek apakah di localhost atau hosting
+        if ($_SERVER['SERVER_NAME'] == 'localhost') {
+            // LOCAL (XAMPP)
+            $this->host = "localhost";
+            $this->user = "root";
+            $this->pass = "";
+            $this->db   = "kelompok8.te24e.my.id";
+        } else {
+            // HOSTING (cPanel)
+            $this->host = "localhost";
+            $this->user = "teemyid_sembarangsek";
+            $this->pass = "bebasaja123";
+            $this->db   = "teemyid_kelompok8";
+        }
+    }
 
     public function koneksi() {
         $koneksi = mysqli_connect($this->host, $this->user, $this->pass, $this->db);
