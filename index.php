4bd6cc50446e142880a34a5074fd622fcdc986fc
Dwi Gusmoro Aji <dwigusmoro14@gmail.com>
2026-04-13 17:17:44 +0700

    update3

--- index.php
+++ index.php
@@ -13,7 +13,8 @@ $koneksi = $air->koneksi();
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <title>Login - SB Admin</title>
+        <title>Login - Sistem Manajemen Air</title>
+        <link rel="icon" type="icon.png" href="../assets/img/Icon.png">
         
         <!-- SB Admin CSS -->
         <link href="css/styles.css" rel="stylesheet" />
