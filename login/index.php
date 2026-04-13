6211c2c243d118f9a693ffb5ca899ec1cff6d30e
joharawal <awaljohar0@gmail.com>
2026-04-13 16:42:29 +0700

    update1

--- login/index.php
+++ login/index.php
@@ -23,7 +23,8 @@ $level = strtolower($dt_user[2] ?? '');
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Dashboard - SB Admin</title>
+        <title>Dashboard - Sistem Manajemen Air</title>
+        <link rel="icon" type="icon.png" href="../assets/img/Icon.png">
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
         <link href="../css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
